3b74cf6dcfc87bec3e53d99c805f72dadc16b7b1
Saja <91623920+Watch-Me-Fly@users.noreply.github.com>
2022-09-25 16:19:53 +0200

    edit navbar

--- views/components/navbar.php
+++ views/components/navbar.php
@@ -1,37 +1,44 @@
-<link rel="stylesheet" type="text/css" href="./../../assets/css/navbar.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/navbar.css">
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <div class="container-fluid d-flex flex-row 
-                p-1 align-items-center">
-        <a href="https://index.html" class="btn">
-            <img src="./../../assets/icons/dark-mode/home.png" alt="Home" title="Home" width="20px">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div id="searchbar" class="">
-            <form action="" class="d-flex">
-                <input type="text" name="" id="" class="me-2">
-                <input type="submit" value="Search" class="btn btn-secondary">
-            </form>
-        </div>
-
-        <div class="collapse navbar-collapse " id="navbarNavDropdown">
-            <ul class="navbar-nav d-flex flex-row justify-content-around col-md-8 col-sm-12 ">
-                <li class="nav-item">
-                    <a href="" class="nav-link text-white">Victims</a>
-                </li>
-                <li class="nav-item">
-                    <a href="" class="nav-link text-white">Euology</a>
-                </li>
-                <li class="nav-item">
-                    <a href="" class="nav-link text-white">About</a>
-                </li>
-
-            </ul>
-            <ul class="navbar-nav d-flex flex-row justify-content-end col-md-4 col-sm-12 ">
-                <li class="nav-item me-1">
+        <div class="container-fluid d-flex flex-row 
+                    p-1 align-items-center">
+            <div class="container 
+                        d-flex justify-content-between 
+                        col-sm-12 col-md-1">
+                <a href="https://index.html" class="btn">
+                    <img src="./../../assets/icons/dark-mode/home.png" alt="Home" title="Home" width="20px">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div id="searchbar" class="container col-sm-12 col-md-4">
+                <form action="" class="d-flex justify-content-center">
+                    <input type="text" name="" id="" class="me-2">
+                    <input type="submit" value="Search" class="btn btn-secondary">
+                </form>
+            </div>
+            <div class="collapse navbar-collapse 
+                        col-sm-12 col-md-4 col-lg-4" id="navbarNavDropdown">
+                <ul class="navbar-nav d-flex flex-row justify-content-evenly">
+                    <li class="nav-item">
+                        <a href="" class="nav-link text-white">Victims</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link text-white">Euology</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link text-white">About</a>
+                    </li>
+                </ul>
+            </div>
+            <ul class="navbar-nav 
+                d-flex flex-row justify-content-end 
+                col-sm-12 col-md-3 col-lg-3">
+                <li class="nav-item me-1 ">
                     <a href="" class="btn btn-secondary">
                         <img src="./../../assets/icons/dark-mode/add.png" alt="" width="20px">
                         <span>Add</span>
@@ -44,21 +51,21 @@
                     </a>
                 </li>
                 <li class="nav-item dropdown me-1">
-                    <a class=" dropdown-toggle btn btn-secondary" href="#" role="button" data-bs-toggle="dropdown"
+                    <a class="dropdown-toggle btn btn-secondary" href="#" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
-                        <img src="./../../assets/icons/dark-mode/globe.png" alt="Choose Language"
-                            title="Choose Language" width="20px">
+                        <img src="./../../assets/icons/dark-mode/globe.png" alt="Choose Language" title="Choose Language"
+                            width="20px">
                     </a>
                     <ul class="dropdown-menu bg-secondary">
-                        <li><a class="dropdown-item" href="#"><img
+                        <li><a class="dropdown-item d-flex flex-row justify-content-center" href="#"><img
                                     src="https://img.icons8.com/color/48/000000/great-britain-circular.png"
                                     alt="English" title="English" width="30" />
                             </a></li>
-                        <li><a class="dropdown-item" href="#">
+                        <li><a class="dropdown-item d-flex flex-row justify-content-center" href="#">
                                 <img src="https://img.icons8.com/color/48/000000/france-circular.png" alt="Français"
                                     title="Français" width="30" />
                             </a></li>
-                        <li><a class="dropdown-item" href="#">
+                        <li><a class="dropdown-item d-flex flex-row justify-content-center" href="#">
                                 <img src="https://img.icons8.com/color/48/000000/saudi-arabia-circular.png"
                                     alt="العربية" title="العربية" width="30" />
                             </a></li>
@@ -66,5 +73,4 @@
                 </li>
             </ul>
         </div>
-    </div>
-</nav>
+    </nav>
