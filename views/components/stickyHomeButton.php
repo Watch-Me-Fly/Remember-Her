c880bbc5e35d242f88a60a74b2bbe0d8530b22ab
Saja <91623920+Watch-Me-Fly@users.noreply.github.com>
2022-09-29 22:42:31 +0200

    sticky home button

---
+++ views/components/stickyHomeButton.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" type="text/css" href="./../../../assets/css/stickyHomeButton.css">
+
+
+<div id="stickySideMenu">
+    <a href="header">
+        <img src="./../../assets/icons/home.png" alt="home" title="Home" width="30px">
+    </a>
+</div>
