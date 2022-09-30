18c9a1552dbad9c974f8ef174ea5e03096d2ffa1
Saja <91623920+Watch-Me-Fly@users.noreply.github.com>
2022-09-30 14:47:42 +0200

    few edits

--- views/components/stickyHomeButton.php
+++ views/components/stickyHomeButton.php
@@ -1,8 +1,23 @@
 <link rel="stylesheet" type="text/css" href="./../../../assets/css/stickyHomeButton.css">
 
 
+<?php
+    if ($language == 'en') {
+        $home = 'To the top';
+    } else if ($language == 'fr') {
+        $home = 'Haut de la page';
+    } else if ($language == 'ar') {
+        $home = 'أعلى الصفحة';
+    }
+?>
+
 <div id="stickySideMenu">
     <a href="header">
-        <img src="./../../assets/icons/home.png" alt="home" title="Home" width="30px">
+        <img 
+            src="./../../assets/icons/home.png" 
+            alt=<?= $home ?> 
+            title=<?= $home ?> 
+            width="30px"
+        >
     </a>
 </div>
