948e519e3d17d9451706b3e039e76de972d52a9e
Saja <91623920+Watch-Me-Fly@users.noreply.github.com>
2022-10-04 14:56:11 +0200

    edit home button

--- views/components/bottom.php
+++
@@ -1,7 +0,0 @@
-
-<!-------- 📜 scripts used throught all pages -------->
-    <?php require_once('./../../assets/js/script.php'); ?>
-
-<!-------- closing html tag -------->
-    </body>
-</html>
--- views/components/stickyHomeButton.php
+++ views/components/stickyHomeButton.php
@@ -1,20 +1,24 @@
-<link rel="stylesheet" type="text/css" href="./../../../assets/css/stickyHomeButton.css">
+<link rel="stylesheet" type="text/css" href="./../../assets/css/stickyHomeButton.css">
 
 
 <?php
-    if ($language == 'en') {
-        $home = 'To the top';
-    } else if ($language == 'fr') {
-        $home = 'Haut de la page';
-    } else if ($language == 'ar') {
-        $home = 'أعلى الصفحة';
-    }
+    /**============================================
+     *               ANCHOR language
+     *=============================================**/
+        // if ($language == 'en') {
+        //     $home = 'To the top';
+        // } else if ($language == 'fr') {
+        //     $home = 'Haut de la page';
+        // } else if ($language == 'ar') {
+        //     $home = 'أعلى الصفحة';
+        // }
+    $home = "to the top";
 ?>
 
 <div id="stickySideMenu">
     <a href="header">
         <img 
-            src="./../../assets/icons/home.png" 
+            src="./../../assets/icons/dark-mode/home.png" 
             alt=<?= $home ?> 
             title=<?= $home ?> 
             width="30px"
