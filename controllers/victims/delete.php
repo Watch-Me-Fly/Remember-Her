9bb1cce8e116565423f1d21095013ebb9e3bca3a
Saja <91623920+Watch-Me-Fly@users.noreply.github.com>
2022-10-30 05:33:14 +0100

    delete article

--- controllers/victims/delete.php
+++ controllers/victims/delete.php
@@ -1,22 +1,24 @@
 <?php 
 
+require_once($_SERVER['DOCUMENT_ROOT'].'/database/OOPmethod/murder.CRUD.php');
+
 if (isset($_GET['id']))
 {
-    // FIXME
     /*--------------------------------------------
     *           Delete user's info
     *---------------------------------------------*/
-        $where = ['victim_id' => $_GET['id']];
-        
+
         // read murder table for source id
-        $article = MurderCRUD::readAll($where);
-        $sourceId = $article['sources'];
+        require_once($_SERVER['DOCUMENT_ROOT'].'/controllers/victims/murder.controller.php');
+        $sourceId = $article[0]->sources_id;
+
+        var_dump($sourceId);
         // delete sources
         $deleteSources= MurderCRUD::deleteSources($sourceId);
         // delete article
-        $deleteArticle= MurderCRUD::deleteArticle($where);
+        $deleteArticle= MurderCRUD::deleteArticle(['victim_id' => $_GET['id']]);
 
-        // header('location: /admin/dashboard');
+        header('location: /admin/dashboard');
 }
 else
 {
